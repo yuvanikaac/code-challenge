a63699047071acf2f08ec1a6811ecfc3e99dd892
yuvanikaac <156583870+yuvanikaac@users.noreply.github.com>
2025-07-08 19:28:05 +0530

    Add files via upload

---
+++ problem 15.py
@@ -0,0 +1,17 @@
+'''
+GET A TWO DIGIT NUMBER FROM THE USER AND MAKE THE TEN'S DIGIT AS 1 AND RETURN IT
+'''
+num=int(input("Enter a two digit number:"))
+o=num%10
+result=10+o
+print(result)
+
+
+
+
+
+
+
+
+
+
