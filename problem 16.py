0dc124018cca27f7e19758d0623e30af824a5f1a
yuvanikaac <156583870+yuvanikaac@users.noreply.github.com>
2025-07-09 21:38:07 +0530

    Add files via upload

---
+++ problem 16.py
@@ -0,0 +1,7 @@
+'''
+GET A THREE DIGIT NUMBER FROM USER AND MAKE THE THE ONE'S DIGIT AS 2 AND PRINT THE DIGIT
+'''
+num=int(input("Enter a three digit number"))
+
+result=(((num//10)*10)+2)
+print(result)
