0ea66b444c1f8cf501112a85a26aefc780abb2ec
yuvanikaac <156583870+yuvanikaac@users.noreply.github.com>
2025-07-21 17:14:08 +0530

    Add files via upload

---
+++ LEVEL2/problem 3.py
@@ -0,0 +1,6 @@
+'''GET A NUMBER FROM THE USER CHECK IF THE NUMBER IS LESS THAN 50 .IF YES PRINT 1 ELSE PRINT 0'''
+num=int(input("Enter a number:"))
+if (num<50):
+    print("1")
+else:
+    print("0")
